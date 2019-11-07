f38c2121115d0e5f08b544a9edaf694a29c6d5ee
cs327259852 <327259852@qq.com>
2019-11-07 13:35:33 +0800

    Update begin.py
    添加os库，可顺序执行多个爬虫

--- sinanewsscrapy/begin.py
+++ sinanewsscrapy/begin.py
@@ -1,7 +1,9 @@
-from scrapy import cmdline
-
+# from scrapy import cmdline
+import os
 # 新闻列表爬虫启动
 # cmdline.execute("scrapy crawl sinanewsspider".split())
 
 # 新闻详情爬虫启动
-cmdline.execute("scrapy crawl sinanewsdetailspider".split())
+# cmdline.execute("scrapy crawl sinanewsdetailspider".split())
+os.system('scrapy crawl sinanewsspider')
+os.system('scrapy crawl sinanewsdetailspider')
